5d4d5252c673fcda18f86b66bea7455ae4a22bb0
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-07-28 19:54:52 +0200

    feat: created array of navLinks

---
+++ client/src/data/navLinks.ts
@@ -0,0 +1,8 @@
+export const navLinks = [
+    {path: '/', label: 'Home'},
+    {path: '/projects', label: 'Projects'},
+    {path: '/gallery', label: 'Gallery'},
+    {path: '/login', label: 'LogIn'},
+    {path: '/admin', label: 'Admin'}
+    
+]
