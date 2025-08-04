d300802766257e467db839b9637f6d372cb8d5aa
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-08-04 14:03:43 +0200

    feat: created language files for sv and en

---
+++ client/src/languages/en.ts
@@ -0,0 +1,13 @@
+const en = {
+
+    navBar: {
+        home: 'Home',
+        projects: 'Projects',
+        gallery: 'Gallery',
+        logIn: 'Log in',
+        admin: 'Admin'
+    }
+    
+}
+
+export default en;
---
+++ client/src/languages/sv.ts
@@ -0,0 +1,13 @@
+const sv = {
+
+    navBar: {
+        home: 'Hem',
+        projects: 'Projekt',
+        gallery: 'Galleri',
+        logIn: 'Logga in',
+        admin: 'Admin'
+    }
+    
+}
+
+export default sv;
