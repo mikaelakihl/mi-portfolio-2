fad934143db6c7ab334d96169ab47145af480a64
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-08-07 01:05:07 +0200

    feat: added herosection with image and logo

--- client/src/components/hero/HeroSection.tsx
+++ client/src/components/hero/HeroSection.tsx
@@ -1,10 +1,28 @@
+import { useLocation } from 'react-router-dom';
 import styles from './HeroSection.module.scss';
+
 
 export const HeroSection = () => {
 
+    const location = useLocation();
+    const path = location.pathname;
+
+    const isHome = path === '/';
+    const isLogin = path === '/login';
+
+    const showLargeHero = isHome || isLogin; 
+    const showLogo = isHome; 
+
+    // const heroClass = isHomePage ? styles ['hero--large'] : styles ['hero--small'];
+
     return (
-        <section className={styles.hero_wrapper}>
-            Hej
+        <section className={`${styles.hero_wrapper} ${styles.hero} ${showLargeHero ? styles.hero_large : styles.hero_small}`}>
+            { showLogo && (
+                <img src={'/assets/mi-portfolio-logo.png'} alt='Logotyp' className={styles.logo}/>
+            )}
+            <div>
+                Hej
+            </div>
         </section>
     )
 }
